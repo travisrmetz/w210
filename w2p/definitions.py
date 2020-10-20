ac60617c650b66abad762ea3d3e99016ace431e4
travisrmetz <travisrmetz@gmail.com>
2020-10-20 19:39:08 +0000

    population work

--- w2p/definitions.py
+++ w2p/definitions.py
@@ -1,5 +1,6 @@
-NUMBER_OF_RECORDS=35000
+NUMBER_OF_RECORDS=1000
 OUTPUT_DIR='processed_data/'
-TCE_TABLE_DIR='raw_data/q1_q17_dr25_tce.csv'
+#TCE_TABLE_DIR='raw_data/q1_q17_dr25_tce.csv'
+TCE_TABLE_DIR='full_tce_list_with_disposition_and_av_34032.csv'
 #TCE_TABLE_DIR='raw_data/alt_tce.csv'
 KEPLER_DATA_DIR='raw_data/light_curves'
