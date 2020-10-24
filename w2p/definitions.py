da2ff7e027ff5a6a852b021ab19e0d684a1f4b76
travisrmetz <travisrmetz@gmail.com>
2020-10-24 12:45:43 +0000

    further model work

--- w2p/definitions.py
+++ w2p/definitions.py
@@ -1,7 +1,7 @@
-NUMBER_OF_RECORDS=1000
+NUMBER_OF_RECORDS=5
 OUTPUT_DIR='processed_data/'
 #TCE_TABLE_DIR='raw_data/q1_q17_dr25_tce.csv'
-#TCE_TABLE_DIR='full_tce_list_with_disposition_and_av_34032.csv'
+TCE_TABLE_DIR='full_tce_list_with_disposition_and_av_34032.csv'
 #TCE_TABLE_DIR='raw_data/alt_tce.csv'
-TCE_TABLE_DIR='tce_error.csv'
+#TCE_TABLE_DIR='tce_error.csv'
 KEPLER_DATA_DIR='raw_data/light_curves'
